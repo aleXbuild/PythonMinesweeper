8af7a9b454233ce8f5083d60f8763581e3763a39
aleXbuild <aleksanderskokunow926@gmail.com>
2026-04-15 20:38:14 +0300

    Begun working on mine generator

--- minesweeper.py
+++ minesweeper.py
@@ -1,22 +1,58 @@
+import random
+
 class Minefield:
     def __init__(self, rows, cols):
         self._rows = rows
         self._cols = cols
-        self._field = [[0 for _ in range(cols)] for _ in range(rows)]
+        self._field = [['#' for _ in range(cols)] for _ in range(rows)]
 
     def get_grid_value(self, row, col):
         return self._field[row][col]
 
 class Game:
     def __init__(self):
-        self._rows = input("Rows: ")
-        self._cols = input("Columns: ")
-        self._field = Minefield(int(self._rows), int(self._cols))
+        self.__size_input = input("Grid size: ")
+        self._size = int(self.__size_input)
+        self._field = Minefield(self._size, self._size)
     
     def draw_grid(self):
         print()
-        print(self._field.get_grid_value(0, 0))
+        st = "   "
+        for i in range(self._size):
+            st = st + "     " + str(i + 1)
+        print(st)
 
+        for row in range(self._size):
+            st = "     "
+            if row == 0:
+                for col in range(self._size):
+                    st = st + "______" 
+                print(st)
+
+            st = "     "
+            for col in range(self._size):
+                st = st + "|     "
+            print(st + "|")
+
+            st = "  " + str(row + 1) + "  "
+            for col in range(self._size):
+                st = st + "|  " + str(self._field.get_grid_value(row, col)) + "  "
+            print(st + "|")
+
+            st = "     "
+            for col in range(self._size):
+                st = st + "|_____"
+            print(st + '|')
+
+        print()
+    
+    def generate_mines(self, mine_count):
+        k = 0
+        while k < mine_count:
+            val = random.randint(0, self._size*self._size-1)
+
+            row = val // self._size
+            col = val % self._size
     
 
 
